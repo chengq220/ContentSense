5166ad0bdab3c516f941b47f9cf484229af94959
chengq220 <chengqiu220@gmail.com>
2025-06-11 19:24:35 -0400

    add new dependencies

--- app/apiRouter.py
+++ app/apiRouter.py
@@ -5,3 +5,7 @@ router = APIRouter()
 @router.get("/")
 async def home():
     return {"Message": "This is the start of an amazing project"}
+
+@router.get("/pred")
+async def predict():
+    return {"Message": "you are querying something from the backend"}
