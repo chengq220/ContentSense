3eed25c3cf89078933fa58f2d5ca6f9f974c3918
chengq220 <chengqiu220@gmail.com>
2025-06-11 19:31:06 -0400

    change compose

--- app/apiRouter.py
+++ app/apiRouter.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
+import torch 
 
 router = APIRouter()
 
 @router.get("/")
 async def home():
-    return {"Message": "This is the start of an amazing project"}
+    return {"Message": "This is the start of an amazing project", "cudaisavailable":torch.cuda.is_available()}
 
 @router.get("/pred")
 async def predict():
