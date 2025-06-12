fc5cd495db4558982e79b02b00760967b2604690
chengq220 <chengqiu220@gmail.com>
2025-06-12 19:45:05 -0400

    extract text from page

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,30 +1,8 @@
-// Query the URL of the current active/focused tab
-const queryURL = async () => {
-    let queryOptions = {active:true, lastFocusWindow: true};
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab.url;
-}
-
-// Get the text in the HTML
-function extractContent(html) {
-    return new DOMParser()
-        .parseFromString(html, "text/html")
-        .documentElement.textContent;
-}
-
-// async function test(){
-//     console.log("onload called test function");
-//     let url = await queryURL();
-//     console.log("the tab is ", url);
-// }
-
-// test();
-
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    // Query the URL of the current active/focused tab
     if(message == "get_url"){
         chrome.tabs.query({ active: true, currentWindow: true}, (tab) => {
-            sendResponse({url: tab.url});
+            sendResponse({url: tab[0].url});
         })
     }
     return true;
--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -5,3 +5,23 @@ chrome.runtime.sendMessage('get_url', (response) => {
         console.log('received data', response);
     }
 });
+
+function get_content(){
+    try{
+        var text = document.documentElement.innerText;
+        const processText = text.match(/[^?!.]*[?!.]/g);
+        processText.shift();
+        if(processText.length > 0){
+            console.log(processText[0]);
+
+            console.log('text extracted');
+        }else{
+            console.log("text not extracted");
+        }
+    }catch(error){
+        console.error("Error: Unable to get text from the page");
+    }
+    
+}
+
+get_content();
