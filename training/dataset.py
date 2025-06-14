fcab8b09dd826b9200fce967dcf18be4f135331d
chengq220 <chengqiu220@gmail.com>
2025-06-14 14:31:16 -0400

    set up dataloader

---
+++ training/dataset.py
@@ -0,0 +1,41 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+from transformers import AutoTokenizer
+
+"""
+Dataset object for custom datasets 
+"""
+class ContentDataset(Dataset):
+    def __init__(self, isTrain = True):
+        data = pd.read_json("hf://datasets/mmathys/openai-moderation-api-evaluation/samples-1680.jsonl.gz", lines=True).fillna(0)
+        shuffle = data.sample(frac=1, random_state=42).reset_index(drop=True)
+        split = int(0.8 * shuffle.shape[0])
+        if isTrain:
+            self.df = shuffle.iloc[0:split]
+        else:
+            self.df = shuffle.iloc[split:]
+        self.tokenizer = AutoTokenizer.from_pretrained("KoalaAI/Text-Moderation")
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, idx):
+        item = self.df.iloc[idx]
+        query = item.iloc[0]
+        tokenized_query = self.tokenizer(query, padding="max_length", truncation=True, max_length=42, return_tensors="pt")
+        one_hot = torch.tensor([item.iloc[iidx] for iidx in range(1, len(item))])
+        return tokenized_query["input_ids"].squeeze(0), one_hot
+
+def get_dataloader(batch = 16, isTrain = True):
+    dataset = ContentDataset(isTrain = isTrain)
+    loader = DataLoader(dataset, batch_size=batch, shuffle=True, drop_last=True)
+    return loader
+
+if __name__ == "__main__":
+    loader = get_dataloader()
+    for i, batch in enumerate(loader):
+        text, label = batch
+        print(text)
+        print(label)
+        exit()
---
+++ training/models/KoalaAIDeberta.py
@@ -0,0 +1,21 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import os 
+import torch
+
+class ModerationModel():
+    def __init__(self):
+        exist = os.path.exists("~/.cache/huggingface/models--KoalaAI--Text-Moderation")
+        self.tokenizer = AutoTokenizer.from_pretrained("KoalaAI/Text-Moderation", local_files_only=exist, padding=True)
+        self.model = AutoModelForSequenceClassification.from_pretrained("KoalaAI/Text-Moderation", local_files_only=exist)
+
+    def pred_logit(self, inputs):
+        tokenizedInputs = self.tokenizer(inputs, return_tensors="pt")
+        with torch.no_grad():
+            logits = self.model(**tokenizedInputs).logits
+            return logits
+        
+    def pred_classes(self, inputs):
+        logits = self.pred_logit(inputs)
+        predicted_class_id = logits.argmax(dim=1)
+        pred_class = [self.model.config.id2label[idx.item()] for idx in predicted_class_id]
+        return pred_class
